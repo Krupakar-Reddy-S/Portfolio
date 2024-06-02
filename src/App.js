5477a8d868fdcba58bb68e17ebd7832a16ae55e6
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-06-02 23:52:58 +0530

    Main App Built with Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Interests from './components/Interests';
+import Project from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
+  const projects = [
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/Image-Editor-Terminal',
+      language: 'Java',
+      title: 'Imag-Editor-Terminal',
+      description: 'Terminal based Image Editor',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/Image-Editor-Terminal'
+    },
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/Web-Scraper-Product-Prices-Comparison',
+      language: 'Python',
+      title: 'Web-Scraper-Product-Prices-Comparison',
+      description: 'Contains a package to extract products from search results of Amazon, Flipkart and snapdeal (all Indian), with an option to filter for relevant products and show their graph comparison',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/Web-Scraper-Product-Prices-Comparison'
+    },
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/bhashini-web-translator',
+      language: 'JavaScript',
+      title: 'bhashini-web-translator',
+      description: 'Webpage Translator for specialized for Regional Languages. Built using Bhashini APIs',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/bhashini-web-translator'
+    },
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/Gemini-ChatBot',
+      language: 'Python',
+      title: 'Gemini-ChatBot',
+      description: 'A ChatBot powered by Gemini \nhttps://gemini-chatbot-test.streamlit.app/',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/Gemini-ChatBot'
+    },
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/Backend-Project-E-Commerce',
+      language: 'Java',
+      title: 'Backend-Project-E-Commerce',
+      description: 'ECommerce Backend System built with SpringBoot',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/Backend-Project-E-Commerce'
+    },
+    {
+      image: 'https://opengraph.githubassets.com/8396179/Krupakar-Reddy-S/Portfolio',
+      language: 'JavaScript',
+      title: 'Portfolio',
+      description: 'Portfolio Website built with React',
+      githubLink: 'https://github.com/Krupakar-Reddy-S/Portfolio'
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {/* <div className='image'></div> */}
+      <Header />
+      <About />
+      <Portfolio />
+      <Interests />
+      <Project projects={projects} />
+      <Contact />
+      <Footer />
     </div>
   );
 }
