f146c41d2cace2636c5b402a51dc7d2bc86de37d
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-06-02 23:51:37 +0530

    Added Contact Component

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import '../css/Contact.css'
+
+function Contact() {
+  return (
+    <div id="contact">
+        <h2>Contact</h2>
+        <div class="contact-card">
+          <p>Feel free to reach out to me for any queries or collaborations on</p>
+          <div class="social-media-links-container">
+            <a href="#" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-facebook font-white back-facebook zmdi-hc-2x"></i></a>
+            <a href="https://github.com/Krupakar-Reddy-S" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-github font-white back-black zmdi-hc-2x"></i></a>
+            <a href="https://x.com/krupakar_reddy_" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-twitter font-white back-twitter zmdi-hc-2x"></i></a>
+            <a href="https://www.instagram.com/krupakar_reddy_s" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-instagram font-white back-instagram zmdi-hc-2x"></i></a>
+            <a href="#" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-youtube-play font-white back-youtube zmdi-hc-2x"></i></a>
+            <a href="#" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-reddit font-white back-reddit zmdi-hc-2x"></i></a>
+            <a href="https://www.linkedin.com/in/krupakar-reddy-s/" target="_blank" rel="noreferrer" class="social-media-logo"><i class="zmdi zmdi-linkedin font-white back-linkedin zmdi-hc-2x"></i></a>
+          </div>
+        </div>
+    </div>
+  );
+}
+export default Contact;
